b4f7690f262cbff8e60c5fa97d3c647173944bc0
Piotr Mierzejewski <me@piotrmierzejewski.com>
2022-10-06 10:09:23 +0200

    Set iTerm2 config path to dotfiles

--- .config/yadm/macos-customization.sh
+++ .config/yadm/macos-customization.sh
@@ -186,3 +186,12 @@ defaults write com.apple.TextEdit NSFixedPitchFontSize -int 14
 
 # Prevent Photos from opening automatically when devices are plugged in
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+###############################################################################
+# iTerm2                                                                      #
+###############################################################################
+# Specify the preferences directory
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.config/iterm2"
+
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
